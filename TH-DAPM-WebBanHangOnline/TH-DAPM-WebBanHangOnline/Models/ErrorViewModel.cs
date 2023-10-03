03cd31b0ef9c13c0a2429321ece6a6ffd6d481f7
wwoi03 <tuandao8826848@gmail.com>
2023-10-04 00:57:01 +0700

    add project into github

---
+++ TH-DAPM-WebBanHangOnline/TH-DAPM-WebBanHangOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TH_DAPM_WebBanHangOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
