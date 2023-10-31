228378738f6d4d795fa189e8a02c9493a7ced39b
wwoi03 <tuandao8826848@gmail.com>
2023-10-31 22:57:48 +0700

    Update model Comment

---
+++ TH-DAPM-WebBanHangOnline/TH-DAPM-WebBanHangOnline/Models/ClassModel/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace TH_DAPM_WebBanHangOnline.Models.ClassModel
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string CommentContent { get; set; }
+        public double StarRating { get; set; }
+        public DateTime CommentDate { get; set; }
+    }
+}
